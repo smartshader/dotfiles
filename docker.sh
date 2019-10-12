1331a068de86874cd723b531938c71b5e2ccb320
Imad Jerra <smartshader@gmail.com>
2019-10-12 11:18:31 -0400

    Add docker-compose

--- docker.sh
+++ docker.sh
@@ -19,3 +19,8 @@ sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker $USER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
