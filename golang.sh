15fdc5fa492d4328dc08ff5515bfe4d4b678e7c0
Imad Jerra <smartshader@gmail.com>
2019-10-12 15:32:08 -0400

    Update golang.sh

--- golang.sh
+++ golang.sh
@@ -4,3 +4,5 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt update
 sudo apt install -y golang-go
+
+sudo sysctl -w fs.inotify.max_user_watches = 524288
