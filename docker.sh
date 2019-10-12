e5472a065807520f34acdde21f36f04f3d14fd1c
Imad Jerra <smartshader@gmail.com>
2019-10-12 01:54:50 -0400

    Create docker.sh insallation

---
+++ docker.sh
@@ -0,0 +1,19 @@
+sudo apt update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   bionic \
+   stable"
+   
+sudo apt update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
