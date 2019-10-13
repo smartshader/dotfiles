3c7b37e0abafb00eaf06a149fdd478cdf7fbf483
Imad Jerra <smartshader@gmail.com>
2019-10-13 02:17:30 -0400

    Create k8cli.sh

---
+++ k8cli.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install -y apt-transport-https
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update
+sudo apt install -y kubectl
