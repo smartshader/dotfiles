a93a8cd7f695ee4f02c1faf50ed060f3fe422681
Imad Jerra <smartshader@gmail.com>
2019-10-13 02:39:29 -0400

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -3,3 +3,5 @@ sudo apt update
 sudo apt install -y vim zsh git
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sed -i '/^plugins/c\plugins=(git history kubectl)' ~/.zshrc
