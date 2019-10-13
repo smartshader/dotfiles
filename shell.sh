f594d239205654bd8da70180af1fb42038f1cc7e
Imad Jerra <smartshader@gmail.com>
2019-10-13 02:47:25 -0400

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -5,3 +5,5 @@ sudo apt install -y vim zsh git snapd
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 sed -i '/^plugins/c\plugins=(git history kubectl)' ~/.zshrc
+
+sed -i '/^export PATH/c\export PATH=$PATH:$HOME/.pulumi/bin:$HOME/go/bin:/snap/bin' ~/.zshrc
