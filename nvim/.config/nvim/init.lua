222d9c59c8a0a71e8fee8dfdb546864639f54d47
Imad Jerra <smartshader@gmail.com>
2026-03-07 00:24:24 -0500

    setup nerd-tree for vim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -31,10 +31,67 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.updatetime = 250
 
--- leader
+-- leader (must be set before lazy)
 vim.g.mapleader = " "
 
+-- lazy.nvim bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          float = {
+            enable = true,
+            open_win_config = function()
+              local screen_w = vim.opt.columns:get()
+              local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+              local w = math.floor(screen_w * 0.6)
+              local h = math.floor(screen_h * 0.7)
+              return {
+                relative = "editor",
+                border = "rounded",
+                width = w,
+                height = h,
+                row = math.floor((screen_h - h) / 2),
+                col = math.floor((screen_w - w) / 2),
+              }
+            end,
+          },
+        },
+        on_attach = function(bufnr)
+          local api = require("nvim-tree.api")
+          local function opts(desc)
+            return { desc = desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          end
+          vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+          vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+          vim.keymap.set("n", "m", api.fs.create, opts("Create"))
+          vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
+          vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+          vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
+          vim.keymap.set("n", "c", api.fs.copy.node, opts("Copy"))
+          vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+          vim.keymap.set("n", "q", api.tree.close, opts("Close"))
+          vim.keymap.set("n", "<Esc>", api.tree.close, opts("Close"))
+          vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
+          vim.keymap.set("n", "y", api.fs.copy.filename, opts("Copy Name"))
+          vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Hidden"))
+        end,
+      })
+    end,
+  },
+})
+
 -- keymaps
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>")
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
