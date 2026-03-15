4a54bc9549af9cb5eb8835dd917737c374816bc2
Imad Jerra <smartshader@gmail.com>
2026-03-15 15:46:10 +0000

    pass extended commands to tmux

--- scripts/25-tmux.sh
+++ scripts/25-tmux.sh
@@ -3,3 +3,11 @@ if [[ ! -d "$TPM_DIR" ]]; then
   info "Installing tmux plugin manager (TPM)..."
   git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
 fi
+
+# Install TPM plugins if not already installed
+"$TPM_DIR/bin/install_plugins"
+
+# Reload tmux config if running inside tmux
+if [[ -n "$TMUX" ]]; then
+  tmux source-file ~/.tmux.conf
+fi
